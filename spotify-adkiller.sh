SecUpwN/Spotify-AdKiller

commit 8c3f4a37cf80000071219384e582ab46a7af291f

Handle unknown conditions more graciously
An attempt to fix the sporadic mutesdescribed in #24

spotify-adkiller.sh
@@ -391,6 +391,16 @@ automute_continuous(){
       then
           echo "## Keep local playback running ##"
 
+    # any other unknown condition -> restore state
+    else
+      echo "## Restoring state ##"
+      unmute
+      ADMUTE=0
+      PAUSED=0
+      LOCPLAY=0
+      PAUSESIGNAL=0
+      ADFINISHED=0
+
     fi
 }
 
@@ -424,6 +434,14 @@ automute_simple(){
     elif [[ "$AD" = "1" && "$PAUSED" = "0"  && "$ADMUTE" = "0" ]]
       then
           mute
+
+    # any other unknown condition -> restore state
+    else
+      echo "## Restoring state ##"
+      unmute
+      ADMUTE=0
+      PAUSED=0
+      ADFINISHED=0
 
     fi
 }
@@ -479,6 +497,16 @@ automute_interstitial(){
       "$LOCPLAY" = "1" ]]
       then
           echo "## Keep local playback running ##"
+
+    # any other unknown condition -> restore state
+    else
+      echo "## Restoring state ##"
+      unmute
+      ADMUTE=0
+      PAUSED=0
+      LOCPLAY=0
+      PAUSESIGNAL=0
+      ADFINISHED=0
 
     fi
 }

iim05te/spotify-premium-o-caralho

commit 6d7a195cb5d868d6c7c73248a06c6b9dcfbd58d6

Mute multiple ads correctly when in simple operation mode
This was a regression introduced by 8c3f4a37cf80000071219384e582ab46a7af291f. Change should fix #28.

spotify-adkiller.sh
@@ -433,6 +433,11 @@ automute_simple(){
       then
           mute
 
+    # another ad
+    elif [[ "$AD" = "1" && "$PAUSED" = "0"  && "$ADMUTE" = "1" ]]
+      then
+          mute
+
     # any other unknown condition -> restore state
     else
       echo "## Restoring state ##"