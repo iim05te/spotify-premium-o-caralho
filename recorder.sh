903cf20d6c971829011be8d17821654b574e987e
Artur Alves <artur.ze.alves@gmail.com>
2016-10-19 17:45:18 +0100

    Fixed bug which prevented the removal of .wav file

--- recorder.sh
+++ recorder.sh
@@ -4,6 +4,6 @@ killall arecord --quiet
 arecord -c 2 -D pulse_monitor -r 192000 -d $1 --quiet "$2".wav
 # lame -V0 -h --vbr-new $2.wav $2.mp3
 lame -h "$2.wav" "$2.mp3" --quiet
-if [ ! -f "$2.wav" ]; then
+if [ -f "$2.wav" ]; then
     rm -f "$2.wav"
 fi
